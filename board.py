350c8f30d93c3b9d93b945ec484edf89a4517627
Eivind Haug-Warberg <ehaugw@gmail.com>
2023-10-18 04:02:19 +0000

    board: make flak8 compliant

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 from rules import get_groups, get_affecting
 
+
 class Board:
     def __init__(self, string):
         string = string.split("\n")
@@ -55,7 +56,7 @@ class Board:
         change = False
         for r in range(9):
             for c in range(9):
-                b = self.is_sole_posibility_in_group(r,c)
+                b = self.is_sole_posibility_in_group(r, c)
                 change = change or b
 
     def is_sole_posibility_in_group(self, row, col):
@@ -75,7 +76,6 @@ class Board:
                             break
         return change
 
-
     def find_group_subsets(self):
         self.print("starting to find subset")
         pair_contributors = []
@@ -84,13 +84,16 @@ class Board:
             for superset in group_type:
                 for subset in superset:
                     vec_with_posses = sorted([(vec, self.posibilities[vec[0]][vec[1]]) for vec in subset
-                                           if len(self.posibilities[vec[0]][vec[1]]) > 1],
-                                          key=lambda x: len(x[1]))
+                                              if len(self.posibilities[vec[0]][vec[1]]) > 1],
+                                             key=lambda x: len(x[1]))
                     for posibility in vec_with_posses:
                         vecs = [posibility[0]]
                         total_posibilities = set(posibility[1])
 
-                        other_vec_with_posses = sorted([v for v in vec_with_posses if v != posibility], key=lambda x: len(x[1].difference(total_posibilities)))
+                        other_vec_with_posses = sorted(
+                            [v for v in vec_with_posses if v != posibility],
+                            key=lambda x: len(x[1].difference(total_posibilities))
+                        )
                         for other_posibility in other_vec_with_posses:
                             if len(other_posibility[1].difference(total_posibilities)) <= 1:
                                 vecs.append(other_posibility[0])
@@ -101,9 +104,8 @@ class Board:
                                     for pos in set(subset).difference(vecs):
                                         inter = self.posibilities[pos[0]][pos[1]].intersection(total_posibilities)
                                         if len(inter) > 0:
-                                            print("removing (%s) from (%s,%s)" %(inter, pos[0], pos[1]))
+                                            print("removing (%s) from (%s,%s)" % (inter, pos[0], pos[1]))
                                             self.try_remove_posibilities(pos[0], pos[1], total_posibilities)
-                                            # do not ask for sole posibilities it would already be before removing a posibility
                                             did_something = True
         if not did_something:
             self.print("start finding quantum pairs")
@@ -124,7 +126,7 @@ class Board:
                                     if (r, c) not in combined:
                                         inter = self.posibilities[r][c].intersection(posibilities)
                                         if len(inter) > 0:
-                                            print("removing (%s) from (%s,%s)" %(inter, r, c))
+                                            print("removing (%s) from (%s,%s)" % (inter, r, c))
                                             self.try_remove_posibilities(r, c, posibilities)
                                             did_something = True
                         if len(rows) == 2:
@@ -136,7 +138,7 @@ class Board:
                                         print(other)
                                         inter = other.intersection(posibilities)
                                         if len(inter) > 0:
-                                            print("removing (%s) from (%s,%s)" %(inter, r, c))
+                                            print("removing (%s) from (%s,%s)" % (inter, r, c))
                                             self.try_remove_posibilities(r, c, posibilities)
                                             did_something = True
                             #  print("col %s: %s in rows
@@ -148,7 +150,7 @@ class Board:
                 for vecs2 in not_vecs1:
                     not_vecs2 = [v for v in not_vecs1 if v != vecs2]
                     for vecs3 in not_vecs2:
-                        pass # find y wing
+                        pass  # find y wing
         return did_something
 
     def set_value(self, row, col, val):
@@ -167,18 +169,18 @@ class Board:
         if len(p) > 1:
             p -= vals
             if len(p) == 1:
-                self.set_value(row,col, list(p)[0])
+                self.set_value(row, col, list(p)[0])
 
-    def print(self, msg = ""):
+    def print(self, msg=""):
         print("%s\n" % msg)
         print("+---+---+---+")
         for r, row in enumerate(self.board):
             out_string = "|"
             for c, char in enumerate(row):
                 out_string += char
-                if (c+1)%3 == 0:
+                if (c + 1) % 3 == 0:
                     out_string += "|"
             print(out_string)
 
-            if (r+1)%3 == 0:
+            if (r + 1) % 3 == 0:
                 print("+---+---+---+")
